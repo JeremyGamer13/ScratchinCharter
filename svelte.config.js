6079e45bf7220afe2c29dabdc889238da6619e32
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2025-11-02 04:03:28 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,17 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'public',
+            assets: 'public',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        })
+    },
 	preprocess: [mdsvex()],
 	extensions: ['.svelte', '.svx'],
     compilerOptions: {
