7ac2f1a5395f9a624e227edd727a0e11d0563aaa
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2025-11-04 01:16:22 -0700

    Update MenuFile.svelte

--- src/lib/components/TopBar/MenuFile.svelte
+++ src/lib/components/TopBar/MenuFile.svelte
@@ -8,11 +8,25 @@
     import Button, { Label } from '@smui/button';
     
     import Application from "$lib/resources/app.svelte";
+    import SaveState from "$lib/stores/state";
+    import SaveStateLarge from "$lib/stores/state-large";
+    import SMUIPrompts from "$lib/resources/smui-prompts";
+    import MelodiiChart from "$lib/resources/chart";
     
     let props = $props();
     let appTopBar = $derived(props.appTopBar);
 
     let menu = null;
+
+    const projectNew = async () => {
+        const doLoad = await SMUIPrompts.confirm("Are you sure you want to make/load a new project?"
+            + "\n" + "All of your unsaved work will be lost!");
+        if (!doLoad) return;
+
+        $SaveState.newProject = true;
+        $SaveState.chart = MelodiiChart.defaultChart();
+        location.reload();
+    };
 
     const songImport = async () => {
         const fileData = await Application.askForSongBlob();
@@ -27,7 +41,7 @@
     const chartExport = async () => {
         const fileHandle = await window.showSaveFilePicker({
             id: "scratchin-charting-chartexport",
-            suggestedName: "sumidk.json",
+            suggestedName: "chart.json",
             types: [{
                 description: "Melodii Chart",
                 accept: {"application/json": [".json"]}
@@ -45,6 +59,10 @@
 </Button>
 <Menu bind:this={menu}>
     <List>
+        <Item onSMUIAction={projectNew}>
+            <Text>New project...</Text>
+        </Item>
+        <Separator />
         <Item onSMUIAction={chartImport}>
             <Text>Import Chart (.json)</Text>
         </Item>
@@ -54,6 +72,7 @@
         <Item onSMUIAction={() => {}}>
             <Text>Import Voices (folder with audio)</Text>
         </Item>
+        <Separator />
         <Item onSMUIAction={chartExport}>
             <Text>Export Chart (.json)</Text>
         </Item>
